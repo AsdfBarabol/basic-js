316b64253aa1c3679a033e94bdf2bb540d2d9683
Andrey Barabolya <andrey_ba@tut.by>
2023-04-17 23:09:10 +0300

    feat: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  array : [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.array.push(`( ${value} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (position > this.array.length || typeof position !== 'number' || position < 1) {
+      this.array = [];
+      throw new Error(`You can't remove incorrect link!`);
+    } else {
+      this.array.splice(position - 1, 1);
+    }
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse()
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.array.join('~~')
+    this.array = []
+    return result
   }
 };
 
